a97575d9aa21db335b72c98c5e35e91100b82631
HaVH468 <72876762+HaVH468@users.noreply.github.com>
2021-02-02 06:02:22 +0100

    Add files via upload

---
+++ IReflectable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    interface IReflectable
+    {
+        void ReflectX();
+
+        void ReflectY();
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class Point : IReflectable
+    {
+        public readonly decimal X;
+        public readonly decimal Y;
+
+        public Point(decimal x)
+        {
+            this.X = x;
+            this.Y = 0;
+        }
+
+        public Point(decimal x, decimal y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void ReflectX()
+        {
+            decimal x = 1;
+            Point A = new Point(x);
+        }
+
+        public void ReflectY()
+        {
+            decimal x = 1, y = 1;
+            Point A = new Point(x,y);
+        }
+    }
+}
